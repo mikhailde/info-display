0f8343bcc6495bee950e96799961f82bba5222c8
mikhailde <mshchannikov@yandex.ru>
2024-12-14 01:49:17 +0300

    refactor: улучшить структуру и обработку MQTT клиента
    - Вынесена функция подключения к MQTT брокеру `connect_mqtt`, которая возвращает объект клиента.
    - Добавлена обработка ошибок при подключении к брокеру с соответствующими логами.
    - Изменена сигнатура функции `publish_message`, теперь принимает объект клиента как аргумент.
    - Улучшено логирование при публикации сообщений, добавлены статусы отправки.

--- apps/backend/device/app/mqtt_client.py
+++ apps/backend/device/app/mqtt_client.py
@@ -2,37 +2,53 @@ import paho.mqtt.client as mqtt
 import os
 import logging
 
-MQTT_HOST = os.getenv("MQTT_HOST", "mosquitto")
-MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
-DEVICE_ID = "tablo_1"
-
 # Настройка логирования
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-def publish_message(message: str, device_id: str = DEVICE_ID):
+MQTT_HOST = os.getenv("MQTT_HOST", "mosquitto")
+MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
+MQTT_KEEPALIVE = 60
+
+def connect_mqtt() -> mqtt.Client:
+    """
+    Подключается к MQTT брокеру.
+
+    Returns:
+        mqtt.Client: Объект MQTT клиента в случае успешного подключения, None в противном случае.
+    """
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            logger.info(f"Connected to MQTT broker at {MQTT_HOST}:{MQTT_PORT}")
+        else:
+            logger.error(f"Failed to connect to MQTT broker, return code {rc}")
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    try:
+        client.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE)
+        return client
+    except ConnectionRefusedError as e:
+        logger.error(f"Error: Could not connect to MQTT broker: {e}")
+        return None
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {e}")
+        return None
+
+def publish_message(client: mqtt.Client, message: str, device_id: str):
     """
     Публикует сообщение в MQTT топик.
 
     Args:
-        message: Сообщение для публикации.
-        device_id: Идентификатор устройства (топика). По умолчанию - "tablo_1".
+        client (mqtt.Client): Объект MQTT клиента.
+        message (str): Сообщение для публикации.
+        device_id (str): Идентификатор устройства (топика).
     """
-    client = mqtt.Client()
-    try:
-        client.connect(MQTT_HOST, MQTT_PORT, 60)
-        logger.info(f"Connected to MQTT broker at {MQTT_HOST}:{MQTT_PORT}")
-
-        result = client.publish(f"device/{device_id}/command", message)
-        logger.info(f"Publishing message '{message}' to topic 'device/{device_id}/command'")
-
-        if result.rc == mqtt.MQTT_ERR_SUCCESS:
-            logger.info(f"Message published successfully with message ID: {result.mid}")
-        else:
-            logger.error(f"Failed to publish message. Return code: {result.rc}")
-
-        client.disconnect()
-    except ConnectionRefusedError as e:
-        logger.error(f"Error: Could not connect to MQTT broker: {e}")
-    except Exception as e:
-        logger.error(f"An unexpected error occurred: {e}")
+    topic = f"device/{device_id}/command"
+    result = client.publish(topic, message)
+    # result: [0, 1]
+    status = result[0]
+    if status == 0:
+        logger.info(f"Message `{message}` sent to topic `{topic}`")
+    else:
+        logger.error(f"Failed to send message to topic {topic}")
